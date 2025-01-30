5f0b8bdd6c08ae27c7f171afc84117e71e1f877e
Lucas Xu <xuhaozhe51@gmail.com>
2025-01-30 11:21:51 -0800

    changed_3

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -1,12 +1,29 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class BallController : MonoBehaviour
 {
+    [SerializeField] private float force = 1f;
+    [SerializeField] private InputManager inputManager;
+
+    private bool isBallLanched; 
+    private Rigidbody ballRB;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        ballRB = GetComponent<Rigidbody>();
+        inputManager.OnSpacePressed.AddListener(LaunchBall);
     }
+
+    private void LaunchBall()
+    {
+        if (isBallLanched) return;
+        isBallLanched = true;
+        ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
+    }
+
+
 
     // Update is called once per frame
     void Update()
