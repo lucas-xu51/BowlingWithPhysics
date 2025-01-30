2fca99166a442d1c1f3f8059b8001f4ce5a66c09
Lucas Xu <xuhaozhe51@gmail.com>
2025-01-30 08:56:27 -0800

    change_2

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -1,16 +1,17 @@
+using System;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+
+public class InputManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
+    public UnityEvent OnSpacePressed = new UnityEvent();
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            OnSpacePressed?.Invoke();
+        }
     }
 }
