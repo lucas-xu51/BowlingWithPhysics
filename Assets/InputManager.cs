ed420ca40530ddbb5d559f7c9c1d3997f2e8c5b1
Lucas Xu <xuhaozhe51@gmail.com>
2025-01-31 13:04:17 -0800

    changed_9

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.Events;
 
 public class InputManager : MonoBehaviour
 {
+    public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
     public UnityEvent OnSpacePressed = new UnityEvent();
     // Update is called once per frame
     void Update()
@@ -13,5 +14,16 @@ public class InputManager : MonoBehaviour
         {
             OnSpacePressed?.Invoke();
         }
+
+        Vector2 input = Vector2.zero;
+        if (Input.GetKey(KeyCode.A))
+        {
+            input += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            input += Vector2.right;
+        }
+        OnMove?.Invoke(input);
     }
 }
